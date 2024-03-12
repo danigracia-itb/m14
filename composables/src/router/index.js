caaf0145d218dac1ec51479c7e8bc2abc05ddc57
danigracia-itb <daniel.gracia.7e6@itb.cat>
2024-03-12 20:02:25 +0100

    composables capacitator

--- composables/src/router/index.js
+++ composables/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Post from "../views/Post.vue";
+import Test from "../views/Test.vue";
 
 const routes = [
+  { path: "/test", name: "Test", component: Test },
   { path: "/", name: "Home", component: Home },
   { path: "/post/:id", name: "Post", component: Post },
 ];
---
+++ composables/src/views/Test.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <h1>Geolocation</h1>
+      <p>Your location is:</p>
+      <p>Latitude: {{ loc.lat }}</p>
+      <p>Longitude: {{ loc.long }}</p>
+    
+      <button @click="getCurrentPosition">
+        Get Current Location
+      </button>
+    </div>
+    </template>
+    
+    <script>
+    import { defineComponent, ref } from 'vue';
+    import { Geolocation } from '@capacitor/geolocation';
+    export default defineComponent({
+      setup() {
+        const loc = ({
+          lat: null,
+          long: null,
+        });
+    
+        const getCurrentPosition = async () => {
+          const pos = await Geolocation.getCurrentPosition();
+          loc.value = {
+            lat: pos.coords.latitude,
+            long: pos.coords.longitude,
+          };
+        };
+        return { getCurrentPosition, loc };
+      },
+    });
+    </script>
