cdf4f58f74cd829569714daa3636ea3c923a3305
danigracia-itb <daniel.gracia.7e6@itb.cat>
2024-03-21 18:31:51 +0100

    ionic layout

---
+++ ionic-layout/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ion-app>
+    <ion-page>
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Estructura Básica</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-content class="ion-padding">
+        <h1>Hello World</h1>
+      </ion-content>
+
+      <ion-footer>
+        <ion-title>App Layout sample</ion-title>
+      </ion-footer>
+    </ion-page>
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonApp, IonPage, IonHeader, IonContent, IonFooter, IonToolbar, IonTitle } from '@ionic/vue';
+</script>
+
+<style scoped></style>
